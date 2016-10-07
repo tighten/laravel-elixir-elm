3f8688d0607fd7c5de26aa3a4eb9ec2f0fd0f3b0
Logan Henson <logan@loganhenson.com>
2016-10-07 08:48:01 -0500

    handle errors/output

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const Elixir = require('laravel-elixir');
-const shell = require('gulp-shell');
+const exec = require('child_process').exec;
+const Q = require('q');
 
 Elixir.extend('elm', (dir) => {
     const elmPath = `${Elixir.config.assetsPath}/elm/${dir}`;
@@ -7,9 +8,13 @@ Elixir.extend('elm', (dir) => {
     return new Elixir.Task('elm', () => {
         const command = `elm make main.elm --output ../../../../${Elixir.config.publicPath}/js/${dir}.js`;
 
-        return gulp.src('')
-            .pipe(
-                shell(`cd ${elmPath}; ${command}`)
-            );
+        let deferred = Q.defer();
+
+        return exec(`cd ${elmPath}; ${command}`, (err, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+
+            deferred.resolve();
+        });
     }).watch(`${elmPath}/main.elm`);
 });
